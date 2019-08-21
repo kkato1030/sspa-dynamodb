37366654823825ac6fd911aaf36f582cc653ea42
KKato <kazuya.kato@serverworks.co.jp>
2019-08-22 01:43:38 +0900

    config.jsを追加

---
+++ static/config.js
@@ -0,0 +1,20 @@
+const keys = [
+  {
+    'name': 'id',
+    'type': 'number',
+    'min': 1
+  },
+  {
+    'name': 'name',
+    'type': 'text'
+  },
+  {
+    'name': 'remarks',
+    'type': 'text'
+  },
+  {
+    'name': 'count',
+    'type': 'number',
+    'min': 0
+  }
+]
